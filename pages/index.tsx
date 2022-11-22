56da5d3b9c94d811f2076ac0f719e2c201c23d3e
mzoric33 <zoricmarin4@gmail.com>
2022-11-22 19:45:06 +0100

    background-circles

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,9 +1,10 @@
 import Head from "next/head";
 import Header from "../components/Header";
+import Hero from "../components/Hero";
 
 export default function Home() {
   return (
-    <div>
+    <div className="bg-[rgb(36,36,36)] text-white h-screen snap-y snap-mandatory overflow-scroll z-0">
       <Head>
         <title>Marin Portfolio</title>
       </Head>
@@ -11,6 +12,9 @@ export default function Home() {
       <Header />
 
       {/* Hero */}
+      <section id="hero" className="snap-center">
+        <Hero />
+      </section>
 
       {/* About */}
 
