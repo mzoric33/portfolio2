179d3f70563cc4cbb1bc0058b986e2c5db35c00a
mzoric33 <zoricmarin4@gmail.com>
2022-11-24 17:37:15 +0100

    experience section

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,6 +2,7 @@ import Head from "next/head";
 import About from "../components/About";
 import Header from "../components/Header";
 import Hero from "../components/Hero";
+import WorkExperience from "../components/WorkExperience";
 
 export default function Home() {
   return (
@@ -18,11 +19,14 @@ export default function Home() {
       </section>
 
       {/* About */}
-      <section id="#about" className="snap-center">
+      <section id="about" className="snap-center">
         <About />
       </section>
 
       {/* Experience */}
+      <section id="experience" className="snap-center">
+        <WorkExperience />
+      </section>
 
       {/* Skills */}
 
